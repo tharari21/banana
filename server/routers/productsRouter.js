8979435cc2866f23bf2267fd87c49fe9bbaba369
Tomer Harari <tharari21@gmail.com>
2022-07-29 16:54:26 -0400

    Added get post and patch for products

--- server/routers/productsRouter.js
+++ server/routers/productsRouter.js
@@ -6,21 +6,61 @@ router.get('/', async (req, res) => {
     // return all products
     try {
         const products = await pool.query('SELECT * FROM products');
+        if (products.rowCount > 0 ) {
+            res.json({products: products.rows})
+        } else {
+            res.send('No Products')
+        }
     } catch (err) {
         console.error(err)
-    }
-    if (products.rowCount > 0 ) {
-        res.json({products: products.rows})
+        res.send(JSON.stringify(err))
     }
 })
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // Create new product
-})
-router.patch('/:productId', (req, res) => {
-    // update req.params.id
-})
+    const {name, description, sellerId, categoryId} = req.body;
+    try {
+        const newProduct = await pool.query(
+            "INSERT INTO products (name, description, seller_id, catagory_id) VALUES($1, $2, (SELECT id FROM users WHERE id=$3), (SELECT id FROM catagories WHERE id=$4))",
+            [name, description, sellerId, catagoryId]
+            );
+            res.json({ message: `Added ${name} to database` })
+            
+        } catch (err) {
+            res.json({ message: JSON.stringify(err)})
+        }
+    })
+    router.patch('/:productId',async  (req, res) => {
+      // update req.params.id
+      const { name, description, sellerId, categoryId } = req.body;
+      const responseObj = {};
+      if (name) {
+          const updatedProduct = await pool.query('UPDATE products SET name=$1 WHERE id=$2', [name, req.params.productId])
+          console.log(updatedProduct)
+          responseObj.name = name
+      }
+      if (description) {
+        const updatedProduct = await pool.query(
+            "UPDATE products SET description=$1 WHERE id=$2",
+            [description, req.params.productId]
+        );
+        console.log(updatedProduct);
+        responseObj.description = description;
+      }
+      if (categoryId) {
+          const updatedProduct = await pool.query(
+              "UPDATE products SET catagory_id=(SELECT id FROM catagories WHERE id=$1) WHERE id=$2",
+              [categoryId, req.params.productId]
+              );
+          console.log(updatedProduct)
+          responseObj.categoryId = categoryId;
+      }
+      res.json(responseObj);
+
+    })
 router.delete('/:productId', (req, res) => {
     // delete req.params.id
+
 
 })
 
