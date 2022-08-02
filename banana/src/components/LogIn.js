d57dc2b5acc125c682ed4ed7edfa8554c681fc2d
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-08-02 09:25:12 -0400

    added styling to login component

--- banana/src/components/LogIn.js
+++ banana/src/components/LogIn.js
@@ -1,16 +1,59 @@
 const LogInComponent = () => {
     return (
-      <div>
+      <div
+        style={{
+          border: "1px solid rgba(0, 0, 0, 0.263)",
+          height: "500px",
+          width: "450px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          postion: "relative",
+          borderRadius: "10px",
+        }}
+      >
         <header>
-          <button>X</button>
-          <h4>Login or Sign Up</h4>
-          <h3>Welcome to Banana</h3>
-          <form action="">
-            <input type="email" />
-            <input type="password" />
-            <button>LOGIN</button>
+          <div style={{ textAlign: "center" }}>
+            <h4 style={{ fontSize: "2rem" }}>LOGIN</h4>
+            <h3>Welcome to Banana</h3>
+          </div>
+          <form
+            action=""
+            style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+          >
+            <input
+              type="email"
+              placeholder="Email"
+              style={{
+                height: "30px",
+                width: "250px",
+                borderRadius: "5px",
+                border: "1px solid rgba(0, 0, 0, 0.263)",
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              style={{
+                height: "30px",
+                width: "250px",
+                borderRadius: "5px",
+                border: "1px solid rgba(0, 0, 0, 0.263)",
+              }}
+            />
+            <button
+              style={{
+                height: "30px",
+                width: "250px",
+                borderRadius: "5px",
+                border: "1px solid rgba(0, 0, 0, 0.263)",
+                backgroundColor: "transparent",
+              }}
+            >
+              SIGN UP
+            </button>
           </form>
-          <span>
+          <span style={{ display: "flex", gap: "10px", alignItems: "center" }}>
             <p>Need an account?</p>
             <a href="">SIGN UP</a>
           </span>
