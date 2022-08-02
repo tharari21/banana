8f22505ad42dd6c760d87a2a21e4cd9539e0d143
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-08-02 15:47:21 -0400

    routes set up for products"

--- banana/src/components/ProductCard.js
+++ banana/src/components/ProductCard.js
@@ -1,15 +1,16 @@
 import {useState, useEffect} from 'react'
+import { BrowserRouter as Router, Route, NavLink, Switch, Link } from "react-router-dom";
 
-const ProductCard = ({product}) => {
-    const [images, setImages] = useState([])
-    useEffect(() => {
-        const getImages = async () => {
-            const req = await fetch(`http://localhost:5000/products/${product.id}/images`);
-            const res = await req.json();
-            setImages(res)
-        }
-        getImages()
-    }, [])
+const ProductCard = ({ product, setSelectedProduct }) => {
+  const [images, setImages] = useState([]);
+  // useEffect(() => {
+  //     const getImages = async () => {
+  //         const req = await fetch(`http://localhost:5000/products/${product.id}/images`);
+  //         const res = await req.json();
+  //         setImages(res)
+  //     }
+  //     getImages()
+  // }, [])
   // return (
   //   <div style={{ margin: '0 2em'}}>
   //       <h1>{product.name}</h1>
@@ -17,46 +18,53 @@ const ProductCard = ({product}) => {
   //   </div>
   // )
   return (
-    <div
-      style={{
-        width: "300px",
-        display: "flex",
-        flexDirection: "column",
-        padding: "1em",
-        borderRadius: "10px",
-        boxShadow: "3px 3px 3px 3px rgba(0, 0, 0, 0.21)",
-        backgroundColor: "black",
-        color: "white",
-        margin: "1em 0",
-      }}
-    >
-      <img
-        style={{ maxWidth: "22em" }}
-        src={
-          images[0]?.url ||
-          "https://media.istockphoto.com/photos/chimpanzee-picture-id170615395?k=20&m=170615395&s=612x612&w=0&h=CswkxjdPpmDOF7FLJ64nuaYrQzsreIS2Ph9KV4ln15g="
-        }
-        alt=""
-      />
-      <div style={{ padding: "1em 0em 0.5em 0.5em" }}>
-        {" "}
-        <span style={{ display: "flex", gap: "3em" }}>
-          <p>{product.name}</p>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "colomn",
-              alignItems: "center",
-              gap: "5px",
-            }}
-          >
-            <ion-icon name="star"></ion-icon>
-            <p>{"product rating"}</p>
+    <Router>
+      <Link to={`/products/${product.id}`}>
+        <div
+          style={{
+            width: "300px",
+            display: "flex",
+            flexDirection: "column",
+            padding: "1em",
+            borderRadius: "10px",
+            boxShadow: "3px 3px 3px 3px rgba(0, 0, 0, 0.21)",
+            backgroundColor: "black",
+            color: "white",
+            margin: "1em 0",
+          }}
+          onClick={(e) => {
+            setSelectedProduct(product);
+          }}
+        >
+          <img
+            style={{ maxWidth: "22em" }}
+            src={
+              images[0]?.url ||
+              "https://media.istockphoto.com/photos/chimpanzee-picture-id170615395?k=20&m=170615395&s=612x612&w=0&h=CswkxjdPpmDOF7FLJ64nuaYrQzsreIS2Ph9KV4ln15g="
+            }
+            alt=""
+          />
+          <div style={{ padding: "1em 0em 0.5em 0.5em" }}>
+            {" "}
+            <span style={{ display: "flex", gap: "3em" }}>
+              <p>{product.name}</p>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "colomn",
+                  alignItems: "center",
+                  gap: "5px",
+                }}
+              >
+                <ion-icon name="star"></ion-icon>
+                <p>{"product rating"}</p>
+              </div>
+            </span>
+            <h3>{"product Price"}</h3>
           </div>
-        </span>
-        <h3>{"product Price"}</h3>
-      </div>
-    </div>
+        </div>
+      </Link>
+    </Router>
   );
 };
 
--- banana/src/components/ProductContainer.js
+++ banana/src/components/ProductContainer.js
@@ -1,32 +1,48 @@
 
 import {useState, useEffect} from 'react';
 import ProductCard from './ProductCard'
-const ProductContainer = () => {
-    const [products, setProducts] = useState([])
-    useEffect(() => {
-        const getProducts = async () => {
-            const req = await fetch('http://localhost:5000/products');
-            const res = await req.json();
-            setProducts(res);
-        }
-        getProducts();
-    }, [])
-
-    return (
-        <div
-    style={{
-      height: "auto",
-      width: "100%",
-      backgroundColor: "#fed13056",
-      color: "black",
-      padding: '1em'
-    }}
-  >
-            <div style={{ marginLeft: "20%", display: 'flex', flexWrap: 'wrap', gap: '2em'}}>
-
-                    {products.map((product) => <ProductCard key={product.id} product={product}/>)}
-            </div>
+const ProductContainer = ({ setSelectedProduct }) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      const req = await fetch("http://localhost:5000/products");
+      const res = await req.json();
+      setProducts(res);
+    };
+    getProducts();
+  }, []);
+  const product = {
+    id: 1,
+    name: "thing",
+    url: "thing.png",
+    rating: 5,
+    price:100.00
+  };
+  return (
+    <div
+      style={{
+        height: "auto",
+        width: "100%",
+        backgroundColor: "#fed13056",
+        color: "black",
+        padding: "1em",
+      }}
+    >
+      <div
+        style={{
+          marginLeft: "20%",
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "2em",
+        }}
+      >
+        <ProductCard
+          product={product}
+          setSelectedProduct={setSelectedProduct}
+        />
+        {/* {products.map((product) => <ProductCard key={product.id} product={product}/>)} */}
+      </div>
     </div>
-    )
-}
+  );
+};
 export default ProductContainer;
