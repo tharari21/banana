7e7a86ac13615ca1598e5530ece2523c9940b130
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-08-03 14:04:53 -0400

    thiung

--- banana/src/components/SearchItem.js
+++ banana/src/components/SearchItem.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route, NavLink, Switch, Link } from "react-router-dom";
 const SearchItem = () => {
+    //{product}
     return (
         <NavLink to={`/products/`} style={{color: 'black', border: '1px solid rgba(0,0,0,0.1)',borderRadius:'5px', display: 'flex', width: '70%', height: '200px'}}>
             <img style={{width: '250px'}} src="https://m.media-amazon.com/images/I/81svbcgk6LL._AC_UY218_.jpg" alt="" />
             <div style={{marginLeft: '20px'}}>
-                <h3 style={{fontSize: '2rem'}}>The best Camera on the market</h3>
-                <p style={{margin: '0'}}>★★★★★</p>
+                <h3 style={{fontSize: '2rem'}}>{'product.name'}</h3>
+                <p style={{margin: '0'}}>{'product.rating'}</p>
                 <span style={{display: 'flex', alignItems: 'center', margin: '0'}}>
                 <p>$</p>
-                <p style={{fontSize: '2rem'}}>1000</p>
+                <p style={{fontSize: '2rem'}}>{"product.price"}</p>
                 </span>
             </div>
         </NavLink>
--- banana/src/pages/SearchPage.js
+++ banana/src/pages/SearchPage.js
@@ -1,9 +1,18 @@
 import SearchItem from "../components/SearchItem"
 
 const SearchPage = () => {
+
+//useEffect to get products and to get catagories
+//products map return SearchItem
+//search item will takes props 
+//catagories map return option
+
     return (
 <div style={{marginLeft: '10%'}}>
     <nav style={{display: 'flex', justifyContent: 'center', marginTop: '5rem'}}>
+        <select name="catagory-filter" id="">
+        
+        </select>
         <input type="text" style={{height: '30px', width: '600px', border:'1px solid rgba(0,0,0, 0.1)',borderRight: 'none', borderTopLeftRadius: '5px',borderBottomLeftRadius: '5px'}}/>
         <button style={{backgroundColor: 'rgb(252, 225, 128)', margin: '0', border:'1px solid rgba(0,0,0, 0.1)',borderLeft: 'none', borderTopRightRadius: '5px',borderBottomRightRadius: '5px'}}><ion-icon name="search-outline"></ion-icon></button>
     </nav>
