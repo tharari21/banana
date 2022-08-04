d7f612fa2627d58be3ee5a3b2d96454fe1c6a363
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-08-04 19:42:40 -0400

    styling search area

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const cors = require('cors')
+const stripe = require('stripe')()
+//STRIPE_PRIVATE_KEY
+// on checkout btn add post request to server for items
+//items: {[{id: , quantity}]}
+//server will return url
+//window.location = url
 
+//add end point for post request 
+// const stripe.checkout.sessions.create({
+//payment_methode_types: ['card],
+//mode: 'payment',
+//success_url: ,
+//cancel_url: ,
+//line_items: req.body.items.map(item => {const storeItem = requestedItems.get(item.id)
+//return {
+//  price_data: {
+//  currency: 'usd',
+//  product_data: {
+    //nameL storeItem.name
+//},
+//unit_amount: storeItem.priceInCents
+//},
+// quantitiy: item.quantity
+//}})
+//})
+//res.json({url: session.url})
 const productsRouter = require('./routers/productsRouter')
 const categoriesRouter = require("./routers/categoriesRouter");
 const cartRouter = require("./routers/cartRouter");
