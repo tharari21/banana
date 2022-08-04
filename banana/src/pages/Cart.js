0517b4cd3fcaf9ff26f605811e6ec607b1913803
Tomer Harari <tharari21@gmail.com>
2022-08-04 12:10:57 -0400

    Cannot access cart if not logged in

--- banana/src/pages/Cart.js
+++ banana/src/pages/Cart.js
@@ -1,28 +1,29 @@
 import { useState, useContext, useEffect} from "react"
 import CartContainer from "../components/CartContainer"
 import CartPurchaseBox from "../components/CartPurchaseBox"
-
+import {Redirect} from 'react-router-dom';
 import { UserContext } from "./App"
 
 const Cart = () => {
     const {user, setUser} = useContext(UserContext)
-    console.log(user)
     const [cart, setCart] = useState(null)
     useEffect(() => {
         const getCart = async () => {
-            const req = await fetch(`http://10.129.2.168:5000/cart?userId=${user.user.id}`)
-            const res = await req.json();
-
-            console.log(res)
-            setCart(res)
+            if (user.user) {
+                const req = await fetch(`http://10.129.2.168:5000/cart?userId=${user.user.id}`)
+                const res = await req.json();
+    
+                console.log(res)
+                setCart(res)
+            }
         }
         getCart()
     }, [])
-    console.log(cart)
     const [numItemsInCart, setNumItemsInCart] = useState(0)
 
     return (
         <div style={{marginLeft: '20%', width: '60%', color: 'black'}}>
+            {!user?.user && <Redirect to="/login"/>}
              <div style={{color: 'black', marginTop:'20%'}}>
              <h1>{numItemsInCart} item in your cart</h1>
             <CartPurchaseBox />
